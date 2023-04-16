cb1c298669b2f32ab3977ec14805f7f3f49cca7e
guyenanh <nguyenanhyoung@gmail.com>
2023-04-16 15:45:19 +0700

    update discount grpc

---
+++ src/Services/Discount/Discount.Grpc/DbContext/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.IRepository
+{
+    public interface IApplicationDbContext
+    {
+        Task<T> GetByIdAsync<T>(string command, object parms);
+        Task<List<T>> GetAllAsync<T>(string command, object parms);
+        Task<int> AddAsync(string command, object parms);
+        Task<int> UpdateAync(string command, object parms);
+        Task<int> DeleteAsync(string command, object parms);
+    }
+}
