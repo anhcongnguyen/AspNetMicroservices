6e4622c2773d833f5ff202852ae195a1fe8d17b5
guyenanh <nguyenanhyoung@gmail.com>
2023-06-06 15:24:18 +0700

    check out feature and validation

--- src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -1,16 +1,27 @@
-﻿using MediatR;
-using System;
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
 {
-    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrderVm>>
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
     {
-        public Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrderByUserName(request.UserName);
+            return _mapper.Map<List<OrdersVm>>(orderList);
         }
     }
 }
